dfc649f8f9904bc20a088479fa895f1d804c1875
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-07-21 20:04:21 +0000

    adjusted SK example to make it run

--- test/TtSemiEvtKit_cfg.py
+++ test/TtSemiEvtKit_cfg.py
@@ -1,57 +1,118 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("TtSemiEvtKit")
+#-------------------------------------------------
+# test cfg file for the use of SK with tqaflayer2
+# event hypotheses
+#-------------------------------------------------
+process = cms.Process("TEST")
+
+## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
+process.MessageLogger.categories = cms.untracked.vstring('TEST')
 
-process.load("TopQuarkAnalysis.Examples.RecoInput_ttSemiMu_cfi")
+#-------------------------------------------------
+# process configuration
+#-------------------------------------------------
 
-# Load pat
-process.load("PhysicsTools.PatAlgos.patLayer0_cff")
-process.load("PhysicsTools.PatAlgos.patLayer1_cff")
-process.load("PhysicsTools.PatAlgos.patLayer1_EventContent_cff")
+## define input
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root')
+)
 
-# Load TQAF layer 2
+## define maximal number of events to loop over
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+## configure process options
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Magnetic field now needs to be in the high-level py
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+
+#-------------------------------------------------
+# tqaf configuration; if you want just to produce 
+# tqafLayer2 on top of an already existing
+# tqafLayer1 just comment the standard tqafLayer1
+# production sequence
+#-------------------------------------------------
+
+## std sequence for tqaf layer1
+process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_full_cff")
+
+## std sequence for tqaf layer2 for semi-leptonic decays
 process.load("TopQuarkAnalysis.TopEventProducers.tqafLayer2_ttSemiLeptonic_cff")
 
-# Load TtSemiEvtKit
+#-------------------------------------------------
+# analyze event with SK
+#-------------------------------------------------
+
+## load TtSemiEvtKit
 process.load("TopQuarkAnalysis.Examples.TtSemiEvtKit_cfi")
 
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('')
-)
-
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(200)
-)
+## register TFileService
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('TtSemiEvtKitHistos.root')
 )
 
+process.p = cms.Path(process.tqafLayer1 *
+                     process.tqafLayer2_ttSemiLeptonic_semiLepFilter *
+                     process.ttSemiEvtKit)
+
+
+#-------------------------------------------------
+# tqaf event content; first ALL objects are
+# dropped in this process; then tqafLayer1,
+# tqafLayer2 and SK content is added
+#-------------------------------------------------
+
+## define event content
+process.tqafEventContent = cms.PSet(
+    outputCommands = cms.untracked.vstring('drop *')
+)
+
+## define tqaf layer1 event content
+process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_EventContent_cff")
+process.tqafEventContent.outputCommands.extend(process.patLayer1EventContent.outputCommands)
+
+## define tqaf layer2 event content
+process.load("TopQuarkAnalysis.TopEventProducers.tqafLayer2_EventContent_cff")
+process.tqafEventContent.outputCommands.extend(process.tqafLayer2TtSemiLeptonicEventContent.outputCommands)
+process.tqafEventContent.outputCommands.extend(['keep *_ttSemiEvtKit_*_*'])
+
+
+#-------------------------------------------------
+# process output; first the event selection is
+# defined: only those events that have passed the
+# full production path are selected and written
+# to file; the event content has been defined
+# above
+#-------------------------------------------------
+
+## define event selection
 process.EventSelection = cms.PSet(
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('p')
     )
 )
-process.patEventContent = cms.PSet(
-    outputCommands = cms.untracked.vstring('drop *')
-)
-process.patEventSelection = cms.PSet(
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('p')
-    )
-)
+
+## configure output module
 process.out = cms.OutputModule("PoolOutputModule",
-    process.patEventSelection,
-    process.patEventContent,
+    process.EventSelection,
+    process.tqafEventContent,
     verbose = cms.untracked.bool(False),
     fileName = cms.untracked.string('TtSemiEvtKitOutput.root')
 )
 
-process.p = cms.Path(process.patLayer0*
-                     process.patLayer1*
-                     process.tqafLayer2_ttSemiLeptonic_semiLepFilter*
-                     process.ttSemiEvtKit
-                     )
+#-------------------------------------------------
+# output paths; in order not to write the
+# persistent output to file comment the the output
+# path
+#-------------------------------------------------
+
+## output
 process.outpath = cms.EndPath(process.out)
-process.patEventContent.outputCommands.extend(process.patLayer1EventContent.outputCommands)
-process.patEventContent.outputCommands.extend(['keep *_TtSemiEvtKit_*_*', 'keep *_solutions_*_*'])
