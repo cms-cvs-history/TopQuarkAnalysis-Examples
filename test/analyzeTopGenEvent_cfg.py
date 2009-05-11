3ed38b67404bcfa8dd7e71acd65f1a3e125f36a7
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-05-11 15:00:46 +0000

    adapt to PATv2

--- test/analyzeTopGenEvent_cfg.py
+++ test/analyzeTopGenEvent_cfg.py
@@ -1,17 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-#-------------------------------------------------
-# test cfg file for tqaflayer1 & 2 production from
-# fullsim for semi-leptonic ttbar events 
-#-------------------------------------------------
 process = cms.Process("TEST")
 
 ## configure message logger
-## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
 ## dump content of TopGenEvent
 process.MessageLogger.categories.append('TopGenEvent:dump')
 ## print final pruned listing of top decay chain
+process.MessageLogger.categories = cms.untracked.vstring('TEST')
 process.MessageLogger.categories.append('TopGenEventAnalyzer::selection')
 process.MessageLogger.cout = cms.untracked.PSet(
  INFO = cms.untracked.PSet(
@@ -19,73 +16,45 @@ process.MessageLogger.cout = cms.untracked.PSet(
   )
 )
 
-#-------------------------------------------------
-# process configuration
-#-------------------------------------------------
-
 ## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-  # PAT test sample for 2.2.X
-   'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_2_X_2008-11-03-STARTUP_V7-AODSIM.100.root'
-   ),
-   #skipEvents = cms.untracked.uint32(300)
+    'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_2_X_2008-11-03-STARTUP_V7-AODSIM.100.root'
+     ),
+     skipEvents = cms.untracked.uint32(0)                            
 )
 
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(100)
 )
-
 
 ## configure process options
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(False)
 )
 
-## configure geometry
+## configure geometry & conditions
 process.load("Configuration.StandardSequences.Geometry_cff")
-
-## configure conditions
+process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = cms.string('STARTUP_V7::All')
 
-# Magnetic field now needs to be in the high-level py
-process.load("Configuration.StandardSequences.MagneticField_cff")
+## std sequence for pat
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
-#-------------------------------------------------
-# tqaf configuration; if the TQAF Layer 1 is
-# already in place you can comment the following
-# two lines
-#-------------------------------------------------
-
-## std sequence for tqaf layer2 ttGenEvent
 process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
-## process.decaySubset.addRadiatedGluons = False
-process.load("TopQuarkAnalysis.TopEventProducers.producers.TtDecaySelection_cfi")
-process.ttDecaySelection.src = "genEvt"
-process.ttDecaySelection.allowedTopDecays.decayBranchA.electron = True
-process.ttDecaySelection.allowedTopDecays.decayBranchA.muon     = True
-process.ttDecaySelection.allowedTopDecays.decayBranchB.electron = True
-process.ttDecaySelection.allowedTopDecays.decayBranchB.muon     = True
-process.ttDecaySelection.allowedTauDecays.leptonic   = False
-process.ttDecaySelection.allowedTauDecays.oneProng   = False
-process.ttDecaySelection.allowedTauDecays.threeProng = False
-## process.p1 = cms.Path(process.makeGenEvt * process.ttDecaySelection)
 
-#-------------------------------------------------
-# analyze genEvent
-#-------------------------------------------------
 from TopQuarkAnalysis.Examples.TopGenEventAnalyzer_cfi import analyzeTopGenEvent
 process.analyzeTopGenEvent = analyzeTopGenEvent
 
-# register TFService
+# register TFileService
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('analyzeTopGenEvent.root')
 )
 
-## analysis path   
-process.p2 = cms.Path(process.makeGenEvt *
-                      #process.ttDecaySelection *
-                      process.analyzeTopGenEvent
-                      )
+## end path   
+process.p1 = cms.Path(process.patDefaultSequence *
+                      process.makeGenEvt *
+                      process.analyzeTopGenEvent)
+
--- test/analyzeTopJet_cfg.py
+++ test/analyzeTopJet_cfg.py
@@ -1,40 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
-#-------------------------------------------------
-# test cfg file for tqaflayer1 & 2 production from
-# fullsim for semi-leptonic ttbar events 
-#-------------------------------------------------
 process = cms.Process("TEST")
 
 ## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-## show JEC from pat jet
-process.MessageLogger.categories.append('TopJetAnalyzer_jec')
-## show JetCorrFactors from pat jet
-process.MessageLogger.categories.append('JetCorrFactors')
-## show JetCorrFactorsProducer from pat jet
-process.MessageLogger.categories.append('JetCorrFactorsProducer')
-process.MessageLogger.cout = cms.untracked.PSet(
- INFO = cms.untracked.PSet(
-   limit = cms.untracked.int32(0),
-  )
-)
-
-#-------------------------------------------------
-# process configuration
-#-------------------------------------------------
+process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
+process.MessageLogger.categories = cms.untracked.vstring('TEST')
 
 ## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-   # PAT test sample for 2.2.X
     'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_2_X_2008-11-03-STARTUP_V7-AODSIM.100.root'
-    )
+     ),
+     skipEvents = cms.untracked.uint32(0)                            
 )
 
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
+    input = cms.untracked.int32(100)
 )
 
 ## configure process options
@@ -42,50 +25,17 @@ process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(False)
 )
 
-## configure geometry
+## configure geometry & conditions
 process.load("Configuration.StandardSequences.Geometry_cff")
-
-## configure conditions
+process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = cms.string('STARTUP_V7::All')
 
-# Magnetic field now needs to be in the high-level py
-process.load("Configuration.StandardSequences.MagneticField_cff")
+## std sequence for pat
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
-
-#-------------------------------------------------
-# tqaf configuration; if the TQAF Layer 1 is
-# already in place you can comment the following
-# two lines
-#-------------------------------------------------
-
-## to apply jet correction factors different from
-## the pat default uncomment this; needs to be
-## called bevor pat/tqafLayer1 is produced
-## process.load("TopQuarkAnalysis.TopObjectProducers.tools.switchJetCorrections_cff")
-
-## std sequence for tqaf layer1
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_cff")
-
-## necessary fixes to run 2.2.X on 2.1.X data
-## comment this when running on samples produced
-## with 22X
-## from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
-## run22XonSummer08AODSIM(process)
-
-#-------------------------------------------------
-# process paths;
-#-------------------------------------------------
-
-process.p0   = cms.Path(process.tqafLayer1)
-
-#-------------------------------------------------
-# analyze jets
-#-------------------------------------------------
 from TopQuarkAnalysis.Examples.TopJetAnalyzer_cfi import analyzeJet
 process.analyzeJet = analyzeJet
-
-process.jetCorrFactors.sampleType = 0
 
 # register TFileService
 process.TFileService = cms.Service("TFileService",
@@ -93,5 +43,6 @@ process.TFileService = cms.Service("TFileService",
 )
 
 ## end path   
-process.p1 = cms.Path(process.analyzeJet)
+process.p1 = cms.Path(process.patDefaultSequence *
+                      process.analyzeJet)
 
